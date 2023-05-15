28490a81072e11329588cca42c2b4204f46e9589
Willian Soares Damasceno <wilhaods@gmail.com>
2023-05-15 15:03:07 -0300

    Update index.jsx
    Login - Front End - Telas em CSS e formulários prontos

--- src/Pages/LoginPage/index.jsx
+++ src/Pages/LoginPage/index.jsx
@@ -1,21 +1,36 @@
-import React from "react";
+import React, {useState} from "react";
 import './styles.css';
 
 const LoginPage = () =>{
+  const [email,setEmail]  = useState("");
+  const [password,setPassword]  = useState("");
+
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log("Submit");
+    console.log("Submit",{email, password});
   }
   return (
     <div className="login-box">
       <h2>Login</h2>
       <form className="form" onSubmit={handleSubmit}>
         <div className="user-box">
-          <input type="email" className="email" id="email" />
+          <input 
+            type="email" 
+            className="email" 
+            id="email" 
+            value={email}
+            onChange={(e)=> setEmail(e.target.value)}
+            />
           <label>Email</label>
         </div>
         <div className="user-box">
-          <input type="password" className="password" id="password"/>
+          <input 
+            type="password" 
+            className="password" 
+            id="password"
+            value={password}
+            onChange={(e)=> setPassword(e.target.value)}
+          />
           <label>Senha</label>
         </div>
         <button>
